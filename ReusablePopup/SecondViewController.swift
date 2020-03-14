ff35c2beb0fb1891a28358be8db2f31f4be66d80
Abhishek Gupta <abhisheknik23@gmail.com>
2020-03-14 23:24:23 +0530

    Initial Commit

---
+++ ReusablePopup/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ReusablePopup
+//
+//  Created by Abhishek Gupta on 14/03/20.
+//  Copyright © 2020 Abhishek Gupta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
